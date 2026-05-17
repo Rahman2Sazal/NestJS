21346a7855cf9559ef4ca4790ee1e7c579b2ff41
Administrator <saz99man.99@gmail.com>
2026-05-17 16:07:11 +0200

    Timing added without any error (basically copy pasted previous steps)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, UseFilters, UseGuards, ForbiddenException, Param } from '@nestjs/common';
 import { ForbiddenExceptionFilter } from './forbidden-exception/forbidden-exception.filter';
 import { StudentGuard } from './student/student.guard';
-import { UppercasePipe } from './uppercase/uppercase.pipe'; // 1. Import your new pipe
+import { UppercasePipe } from './uppercase/uppercase.pipe';
+import { TimingInterceptor } from './timing/timing.interceptor'; // 1. Import your new pipe
 
 @Controller()
 export class AppController {
@@ -22,13 +23,21 @@ export class AppController {
     return 'Welcome to the exclusive student lounge!';
   }
 
-  // 2. Add the dynamic route here and apply the Pipe inside the @Param decorator
+  
   @Get('hello/:name')
   getName(@Param('name', UppercasePipe) name: string) {
-    // The 'name' variable here will already be transformed to uppercase by the time this code runs!
+   
     return `Hello, ${name}!`;
   }
 
+@Get('performance-data')
+  @UseInterceptors(TimingInterceptor)
+  getPerformanceData() {
+    return {
+      message: 'This database call was tracked and clocked!',
+      status: 'success'
+    };
+  }
   @Get('public-data')
   getPublicData() {
     return 'This is public information!';
--- src/timing/timing.interceptor.ts
+++ src/timing/timing.interceptor.ts
@@ -1,9 +1,43 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class TimingInterceptor implements NestInterceptor {
+  
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+   
+    const startTime = Date.now();
+
+   
+    return next.handle().pipe(
+   
+      tap(() => {
+        const duration = Date.now() - startTime;
+        
+        console.log(`Execution time: ${duration}ms`);
+      }),
+      
+     
+      map((data) => {
+        const duration = Date.now() - startTime;
+        
+        
+        if (typeof data === 'object' && data !== null) {
+          return {
+            ...data,
+            meta: {
+              executionTime: `${duration}ms`,
+            },
+          };
+        }
+        
+        
+        return {
+          response: data,
+          meta: { executionTime: `${duration}ms` }
+        };
+      }),
+    );
   }
 }
